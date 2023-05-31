9bf20c0848f3bba7f2c4112439ed83780af6a63c
ayoubjouda <ayoubjouda@hotmail.fr>
2023-05-31 18:30:43 +0100

    Swagger setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -9,6 +9,14 @@ async function bootstrap() {
       whitelist: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Schedulz API")
+    .setDescription("schedulz backend services ")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   app.enableCors({ origin: "*" });
   await app.listen(process.env.PORT || 3333);
